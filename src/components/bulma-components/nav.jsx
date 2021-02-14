76267f700e2accad88cf26ba0c66302769704c1a
juan <juancraft12yt@gmail.com>
2021-02-14 16:57:20 -0300

    Updated main nav

--- src/components/bulma-components/nav.jsx
+++ src/components/bulma-components/nav.jsx
@@ -1,89 +1,39 @@
 import React from "react";
-import { css } from "@emotion/css";
+import { css } from "@emotion/css"
 
-const style = css({
-  postion: "fixed",
-});
+const mainNav = css({
+  padding: "15px",
+  fontFamily: "Roboto",
+  background:"#495057",
+  display: "flex"
+})
 
-const Nav = () => {
+const navList = css({
+  display: "flex",
+  flexGrow: "1"
+})
+
+const listItem = css({
+  margin: "10px"
+})
+
+const navLinks = css({
+  color: "white"
+})
+
+const BulmaNav = () => {
   return (
-    <nav
-      className={`navbar ${style}`}
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-            alt="asdf"
-          />
-        </a>
-
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          href="/#"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item" href="/#">
-            Home
-          </a>
-
-          <a className="navbar-item" href="/#">
-            Documentation
-          </a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link" href="/#">
-              More
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item" href="/#">
-                About
-              </a>
-              <a className="navbar-item" href="/#">
-                Jobs
-              </a>
-              <a className="navbar-item" href="/#">
-                Contact
-              </a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item" href="/#">
-                Report an issue
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary" href="/#">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light" href="/#">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
+    <nav className={mainNav}>
+      <ul className={navList}>
+        <li className={listItem}><a className={navLinks} href="#">Home</a></li>
+        <li className={listItem}><a className={navLinks} href="#">About Us</a></li>
+        <li className={listItem}><a className={navLinks} href="#">The Mission</a></li>
+      </ul>
+      <div>
+        <button className="button" style={{margin: "5px"}} >Sign Up</button>
       </div>
     </nav>
   );
 };
 
-export default Nav;
+export default BulmaNav;
