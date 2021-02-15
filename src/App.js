d4db59e15df5e4f0d96fb7d375de2aa849b8a153
jbovone <nagethys@gmail.com>
2021-02-15 16:00:21 -0300

    dropdown

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import AdoptionsPage from "./pages/adoptionsPage";
 import Form from "./components/Form/Form";
+import Aside from "./components/bulma-components/aside";
 /*
   https://bulma.io/documentation/components/card/
   ojo que los tags img hr estan sin cerrar!
@@ -15,16 +16,56 @@ import Form from "./components/Form/Form";
   >> si se atasca un puerto error: listen EADDRINUSE: address already in use :::3001 
 */
 const pets = [
-  { name: "Coty",
+  {
+    name: "Coty",
     description: "nesecito un duenño yaaa. Tengo dos años",
-    img: 
- },
-  { name: "Perro1" },
-  { name: "Perro1" },
-  { name: "Perro1" },
-  { name: "Perro1" },
-  { name: "Perro1" },
-  { name: "Perro1" },
+    type: "gato",
+    images: "",
+  },
+  {
+    name: "Simmy",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+
+  {
+    name: "Bandit",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+
+  {
+    name: "Coty",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+  {
+    name: "Yo",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+  {
+    name: "Coty",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+  {
+    name: "Coty",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
+  {
+    name: "Coty",
+    description: "nesecito un duenño yaaa. Tengo dos años",
+    type: "gato",
+    images: "",
+  },
 ];
 function App() {
   const [formState, setFormState] = useState({
@@ -35,6 +76,7 @@ function App() {
   return (
     <Fragment>
       <Navigation formShow={setFormState} />
+      <Aside />
       <Router>
         {formState.show && (
           <Form
